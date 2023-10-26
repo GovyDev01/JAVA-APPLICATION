dd5c0259de07e0aa7d48adcf1807a923ba7c6410
GovyDev01 <>
2023-10-27 02:26:48 +0800

    Java Application

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+
+/**
+ *
+ * @author jhonv
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        
+    }
+}
---
+++ src/My_Connection.java
@@ -0,0 +1,37 @@
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
+public class My_Connection {
+    
+    private static String serverName="localhost";
+    private static String username ="root";
+    private static String dbname ="users_db";
+    private static Integer portnumber =3306;
+    private static String password  ="";
+    
+    public static Connection getConnection()
+    {
+        Connection conn = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(serverName);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+         try {
+            conn = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger("Get Connection - >" + My_Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        return conn;
+        
+    }
+}
